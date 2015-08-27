3722794190d1b6d23d7ef4248a9991365f6edfc9
Jabez <jabez.wilson0@gmail.com>
2015-08-27 13:12:50 +1000

    added all command codes to header file and completed page program function

--- CORTEX_ATSAM3X_Atmel_Studio_demo/src/bluesat_lib/components/m25p32/m25p32_drv.c
+++ CORTEX_ATSAM3X_Atmel_Studio_demo/src/bluesat_lib/components/m25p32/m25p32_drv.c
@@ -57,3 +57,32 @@ void m25p32SectorErase(uint32_t addr){
     spi_write_packet(SPI0,&command,ONE_BYTE);
     spi_deselect_device(SPI0,SPI_Device_Memory);
 }
+
+
+void m25p32PageProgram(Byte* data, uint32_t addr,uint32_t dataSize){
+    int8_t command;
+    //sending write enable command
+    spi_select_device(SPI0,SPI_Device_Memory);
+    command = WRITE_ENABLE;
+    spi_write_packet(SPI0,&command,ONE_BYTE);
+    spi_deselect_device(SPI0,SPI_Device_Memory);
+    
+    addr = (addr & 0x00FFFFFF);
+    
+    spi_select_device(SPI0,SPI_Device_Memory);
+    command = PAGE_PROGRAM;
+    spi_write_packet(SPI0,&command,ONE_BYTE);
+    spi_write_packet(SPI0,&addr,3 * ONE_BYTE);
+    int i = dataSize -1;
+    while(i >= 0){
+        spi_write_packet(SPI0,&Byte[i],ONE_BYTE);
+    }
+    spi_deselect_device(SPI0,SPI_Device_Memory);
+    
+    //sending write disable command
+    spi_select_device(SPI0,SPI_Device_Memory);
+    command = WRITE_DISABLE;
+    spi_write_packet(SPI0,&command,ONE_BYTE);
+    spi_deselect_device(SPI0,SPI_Device_Memory);
+}
+}
--- CORTEX_ATSAM3X_Atmel_Studio_demo/src/bluesat_lib/components/m25p32/m25p32_drv.h
+++ CORTEX_ATSAM3X_Atmel_Studio_demo/src/bluesat_lib/components/m25p32/m25p32_drv.h
@@ -14,11 +14,22 @@
 //more Commands need to be defined
 #define WRITE_ENABLE 0x06
 #define WRITE_DISABLE 0x04
-#define BULK_ERASE 0xc7
+//#define READ_IDENTIFICATION_SMALL 0x9e  
+//#define READ_IDENTIFICATION_LARGE 0x9f
+#define READ_STATUS_REGISTER 0x05
+#define WIRTE_STATUS_REGISTER 0x01
+#define READ_DATA_SLOW 0x03
+#define READ_DATA_FAST 0x08
+#define PAGE_PROGRAM 0x02
 #define SECTOR_ERASE 0xd8
+#define BULK_ERASE 0xc7
+#define DEEP_POWER_DOWN 0xb9
+#define RELEASE_FROM_POWER_DOWN 0xab
+#define READ_ELECTRONIC_SIGN 0xab
 
 //Prototypes
 void m25p32BulkErase();
 void m25p32SectorErase(uint32_t addr);
+void m25p32PageProgram(Byte* data, uint32_t addr);
 
 #endif /* M25P32_DRV_H_ */
